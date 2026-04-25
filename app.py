87fb5dc8a17434d1647ada0d265bb05214a57d53
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-04-25 15:03:09 -0400

    corrections

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from ultralytics import YOLO
 from roboflow import Roboflow
 import ultralytics
 from IPython.display import Image
+import os
+from dotenv import load_dotenv
+
+# load environment variables from .env file
+load_dotenv()
+api_key = os.getenv("DATA_SET_API_KEY")
+workspace = os.getenv("ROBOFLOW_WORKSPACE")
+project_name = os.getenv("ROBOFLOW_PROJECT")
+version_number = os.getenv("ROBOFLOW_VERSION")
 
 if __name__ == "__main__":
-    rf = Roboflow(api_key="Qi37FpOq1jfVKwxNKLCl")
-    project = rf.workspace("-jwzpw").project("continuous_fire")
-    version = project.version(1)
+    rf = Roboflow(api_key=api_key)
+    project = rf.workspace(workspace).project(project_name)
+    version = project.version(version_number)
     dataset = version.download("yolov8")
     print(dataset.location)
     model = YOLO("yolov8n.pt")
